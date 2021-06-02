b6550e9078fefe6c14153711f5becba67964a24b
davies3matt <mdldavies@gmail.com>
2021-06-02 22:35:12 +0200

    fix(wavey-text): fixes animation

---
+++ src/components/WaveyText/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+const WaveyText: React.FC = () => {
+    return (
+        <div className="wrapper"
+        >
+            <span>W</span>
+            <span>E</span>
+            <span>E</span>
+            <span>K</span>
+            <span>E</span>
+            <span>N</span>
+            <span>D</span>
+        </div>
+    );
+}
+
+export default WaveyText;
